972c013e0fefd0c66d0922e829dcdce0af788d85
ivanamark <ivana.markovic69@gmail.com>
2020-01-27 05:02:08 +0100

    string using template-literals

---
+++ Lesson1/lesson1/template-literals.js
@@ -0,0 +1,12 @@
+/*
+ * Instructions: Change the `greeting` string to use a template literal.
+ */
+
+const myName = 'Ivana';
+const interest = 'programming';
+const school = "Udacity";
+const greeting = `Hello, my name is ${myName} .
+
+ I am very interested in ${interest} 
+ at  ${school} `;
+console.log(greeting);
