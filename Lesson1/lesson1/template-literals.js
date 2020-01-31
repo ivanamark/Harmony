7a978e121fdbfed5afcc7e69bd89e40e3b0b86bc
ivanamark <ivana.markovic69@gmail.com>
2020-01-31 15:39:40 +0100

    add older es script exaple

--- Lesson1/lesson1/template-literals.js
+++ Lesson1/lesson1/template-literals.js
@@ -5,8 +5,10 @@
 const myName = 'Ivana';
 const interest = 'programming';
 const school = "Udacity";
-const greeting = `Hello, my name is ${myName} .
+const greeting = `    Hello, my name is ${myName} .
 
- I am very interested in ${interest} 
- at  ${school} `;
+    I am very interested in ${interest} at  ${school} `;
 console.log(greeting);
+//earlier
+var myStr="FirstLine\n\t\\SecondLine\n\t\t\\ThirdLine";
+console.log(myStr);
