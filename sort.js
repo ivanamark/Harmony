65858c3d38f6a28d98df2d06ca3f60ecef04d280
ivanamark <ivana.markovic69@gmail.com>
2020-12-06 15:27:58 +0100

    sorting with arrow function

--- sort.js
+++ sort.js
@@ -2,9 +2,7 @@ const strs = ["abc", "", "aaa", "a", "zz"];
 
 function sortByLength(strs) {
     
-    return strs.sort(function(a,b){
-         return a.length-b.length
-});
+    return strs.sort((a,b)=> a.length-b.length)
 
 }
 console.log(sortByLength(strs));
