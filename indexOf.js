72fb3612280696eb27f9bb5b7fad872757541cd9
ivanamark <ivana.markovic69@gmail.com>
2020-12-28 14:44:33 +0100

    using substring and regex

--- indexOf.js
+++ indexOf.js
@@ -5,22 +5,28 @@ Return the index of the first occurrence of “2020” without using the indexOf
 Example:
 $ find2020(‘2220000202220020200’)
 $ 14 */
-function find2020(str){
+function find20201(str1){
     var elem='2020';
-    for(var x=0;x<str.length;x++){ 
+    for(var x=0;x<str1.length;x++){ 
         var y= x+elem.length
-    if(str.substring(x,y) == elem){
+    if(str1.substring(x,y) == elem){
         return x;
     }
     }
-    
+    return -1;
 }
-var result=find2020('2220000202220020200')
-console.log(result)
+var result9=find20201('2220000202220020200')
+console.log(result9)
+
+
 function find2020(str){
     var myRegex=/2020/;
+    if(myRegex.test(str)== false){
+        return -1
+    }
     var result1=str.match(myRegex);
     return result1.index;
+    
     
 }
 
