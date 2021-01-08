684d8e70c40ec8d410ccd880cce124b70c907b4a
ivanamark <ivana.markovic69@gmail.com>
2021-01-08 02:19:16 +0100

    any number of hubs

--- iot.js
+++ iot.js
@@ -4,22 +4,37 @@ Example:
 let network = { switch: [0,1], hub: [[2,1], [2,5]], light: [1,6] }
 $ canToggle(network)
 $ true */
-function  canToggle() {
-    var network={};
-    network.switch=[0,1];
-    network.hub=[[2,1],[2,5]];
-    network.light=[1,6]
-    var elem=Math.sqrt((2-0)^2 + (1-1)^2);
-    console.log(elem);
-    var elem2=Math.sqrt((2-1)^2+(5-6)^2);
-    console.log(elem2);
-    var big=Math.max(elem,elem2);
-    var distance=Math.sqrt((2-2)^2 + (5-1)^2)
+
+
+function  canToggle1() {
+    var network1={};
+    network1.switch=[0,1];
+    network1.hub=[[2,1],[2,5]];
+    network1.light=[1,6]
+    for(var i=0;i<network1.hub.length;i++){
+        var elem=network1.hub[i];
+        console.log(elem)
+        var elem1=Math.sqrt((elem[0]-network1.switch[0])^2 + (elem[1]-network1.switch[1])^2);
+         var elem2=Math.sqrt((elem[0]-network1.light[0])^2 + (elem[1]-network1.light[1])^2);
+         
+         var distance=(Math.sqrt((elem[0][0]-elem[0][1])^2 + (elem[1][0]-elem[1][1])^2))
+         
+         console.log(elem1)
+         console.log(elem2)
+         
+         console.log(distance);
+         if(!(!elem1) && !(!elem2)){ 
+    var big=Math.max(elem1,elem2);
+    console.log(big)
+    }
+    }
+  
     if(distance <= 5*big){
         return true;
     }else{
         return false;
     }
 }
-var result=canToggle();
+var result=canToggle1();
 console.log(result);
+
