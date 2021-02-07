149c7f9002f33fe6af1f4d8aa91d201d31f1813b
ivanamark <ivana.markovic69@gmail.com>
2021-02-07 02:59:47 +0100

    change array match

--- triangle.js
+++ triangle.js
@@ -14,41 +14,38 @@ function isInTriangle(triangle,spot){
     var yu=[];
     for(var i=0;i<triangle.length;i++){
         console.log(triangle[i])
-       if(triangle[i].sort().toString() == spot.sort().toString()){
-
-       console.log(true)
-       break;
-        }else{     
+      
         for(var j=1;j<triangle[i].length;j+=2){
             var elementY= triangle[i][j];
+             console.log(elementX)
               xu.push(Math.max(elementY))
-               
-             
-        }
-        var maxY=Math.max(...xu)
+               var maxY=Math.max(...xu)
         var minY=Math.min(...xu)
-        for(var k=0;k<triangle[i].length;k+=2){
-            var elementX= triangle[i][k];
-           // console.log(elementX)
-             yu.push(Math.max(elementX))
            
         }
-        }
-         var maxX=Math.max(...yu)
+        
+        for(var k=0;k<triangle[i].length;k+=2){
+            var elementX= triangle[i][k];
+           
+             yu.push(Math.max(elementX))
+            var maxX=Math.max(...yu)
          var minX=Math.min(...yu)
-/* console.log((spot[1]<=xu/2))
-console.log((spot[0]<=yu/2) )
-     */ 
-    if((spot[0]<=(maxX+minX)/2) && (spot[1]<=(maxY+minY)/2)){
-           console.log(true) 
+         
+        
+        }
+        
+
+
+    if(((minX<=spot[0])&&(spot[0]<=(maxX+minX)/2)) && ((minY<=spot[1])&&(spot[1]<=(maxY+minY)/2))){
        
-       }else{   
-           console.log(false)
-       }
-    } 
-   
-   
+        
+        console.log(true) 
+       
+    } else{
+        console.log(false) 
+    }
+        }
+        }
     
-}
-var result=isInTriangle([ [4,3], [4,5], [6.5,4] ], [4.5,3.5])
-//console.log(result)
+
+var result=isInTriangle([ [0,0], [0,3], [4,0] ], [3,2])
