25080f08f36b9d4cca7a5f4881d4d4983f09a7c1
ivanamark <ivana.markovic69@gmail.com>
2021-03-12 20:37:47 -0600

    delete empty lines

--- tik-tok.js
+++ tik-tok.js
@@ -15,33 +15,31 @@ $ validTTTPosition(['  O', '   ', '   '])
 $ false */
 function validTTTPosition(array) {
   var newarr=[];
-  for(var i=0;i<array.length;i++){
-    if(array[i]=="XXX" || array[i]=="OOO"){
-      return false;
-    }else{   
-    for(var j=0;j<array.length;j++){
+      for(var i=0;i<array.length;i++){
+          if(array[i]=="XXX" || array[i]=="OOO"){
+             return false;
+          }else{   
+      for(var j=0;j<array.length;j++){
         
         newarr.push(array[i][j]) 
-    }
+          }
     
-    for(var k=0;k<newarr.length;k++){
-      if(newarr[k]==' '){
+      for(var k=0;k<newarr.length;k++){
+          if(newarr[k]==' '){
         newarr[k]='X';
         break;
-      }
-    }
-    
-    for(var t=0;t<newarr.length;t++){
-      if((newarr[0] && newarr[4] && newarr[8]=='X') || (newarr[2] && newarr[4] && newarr[6]=='X')){
-        return true;
-      }
-    }
-  } 
+          }
+        }
+      for(var t=0;t<newarr.length;t++){
+          if((newarr[0] && newarr[4] && newarr[8]=='X') || (newarr[2] && newarr[4] && newarr[6]=='X')){
+            return true;
+          }
+        }
+      } 
   
-  }
+    }
   
-        return false;
-      
+    return false;     
 }
 console.log(validTTTPosition(['XOX', 'O O', 'XOX']));
 console.log(validTTTPosition(['XOX', ' X ', '   ']));
