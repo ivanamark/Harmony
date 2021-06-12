da329a9f29d198705363e81a78650238ae9dcac6
ivanamark <ivana.markovic69@gmail.com>
2021-06-12 09:01:10 -0500

    comparing numbers

---
+++ lonelyNum.js
@@ -0,0 +1,35 @@
+/* This week’s question:
+Given three numbers, return their product. But, if one of the numbers is the same as another, it does not count: If two numbers are similar, return the lonely number. If all numbers are same, return 1.
+
+Example:
+
+$ lonelyNumber(1,2,3)
+$ 6
+
+$ lonelyNumber(6,6,4)
+$ 4
+
+$ lonelyNumber(7,7,7)
+$ 1 */
+function lonelyNumber(a,b,c) {
+    
+    if(a==b && b==c){
+         console.log(1);
+        return 1;
+       
+    }if(a==b && a!=c) {
+         console.log(c);
+        return c
+       
+    }
+     if(a==c && a!=b){
+        console.log(b);
+    }else{
+        console.log(a+b+c);
+        return a+b+c;
+       
+    }
+}
+lonelyNumber(1,2,3);
+lonelyNumber(6,6,4);
+lonelyNumber(7,7,7);
