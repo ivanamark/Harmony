90d0653c00b7fff0f546ccb117b5e6bccc08bedc
ivanamark <ivana.markovic69@gmail.com>
2022-04-16 00:35:07 -0500

    number of sides in regular polygon and area when side equals one

---
+++ interiorAngle.js
@@ -0,0 +1,20 @@
+/* This week’s question:
+Given an integer n representing the number of sides of a regular polygon, return the measure of each interior angle. Bonus points: implement some of the other functions listed in the linked Wikipedia page!
+
+Example:
+
+$ interiorAngleSize(3)
+$ 60 // Each angle in a triangle that is a regular polygon is 60 degrees
+
+$ interiorAngleSize(8)
+$ 135 */
+function interiorAngleSize(n) {
+    return 180*(n-2)/n
+}
+console.log(interiorAngleSize(8))
+//The area A of a convex regular n-sided polygon having side s=1
+function area(n) {
+    return ((n/4)*((Math.tan(3.1416/n))**(-1))).toPrecision(5)
+}
+console.log(area(6))
+
