9b12e94fbe63f3dce371cfaa7269d93aab8b00a5
Ivana Markovic <ivana.markovic69@gmail.com>
2022-05-30 21:56:17 -0500

    Create uniqueString.js

---
+++ uniqueString.js
@@ -0,0 +1,38 @@
+/*This week’s question:
+Write a function that determines
+if all the characters in a given
+string are unique. 
+Can you do this without making any
+new variables?
+You choose if you want to 
+include capitalization in your
+consideration for this one, 
+as a fun challenge.
+
+Example:
+
+> allUnique('Cassidy')
+> false
+
+> allUnique('cat & dog')
+> false
+
+> allUnique('cat+dog')
+> true*/
+
+function allUnique(str){
+   str.toLowerCase();
+   for(let i=0;i<str.length;i++){
+   
+      for(let a=i+1;a<str.length;a++){
+       if(!!(str[i]==str[a])){
+          return false
+       }  
+   }
+   }
+  return true
+}
+//allUnique('cat &dog');
+//allUnique('cat & dog');
+//allUnique('Cassidy')
+allUnique('cat+dog')
